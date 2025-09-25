6c7594c1912d52eb725672233cab8ade5709a7de
Whiteside <vovadxc@gmail.com>
2025-09-25 09:27:48 +0200

    Novy methoda pro vypočet ceny bez DPH

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 class Car:
+
+    """
+    Cenu auta udavame v DPH
+    """
     
     #je možnost kontroly vstupních dat ktere prijdou při instanci do objektu
     def __init__(self, znacka: str,rok: int,model: str,barva: str,typ_prevodovky: str,cena: float): 
@@ -10,7 +14,10 @@ class Car:
         self.cena = cena
 
     def Vypis(self):
-        return f"Nazev auta: {self.znacka};\nRok výroby: {self.rok};\nModel: {self.model};\nBarva: {self.barva};\nTyp prevodovky: {self.typ_prevodovky};\nCena: {self.cena}"
+        return f"Nazev auta: {self.znacka}\nRok výroby: {self.rok}\nModel: {self.model}\nBarva: {self.barva}\nTyp prevodovky: {self.typ_prevodovky}\nCena včetně DPH: {self.cena} Kč\n"
+
+    def cena_bez_dph(self):
+        return f"Nazev auta: {self.znacka} \nCena bez DPH: {(self.cena * 0.79)}"
 
 audi = Car("Audi",1999, "A4", "Stříbrna", "Manuální", 45000)
 skoda = Car("Škoda", 2014, "Superb II", "Bíla", "Manuální", 310000)
@@ -18,3 +25,4 @@ skoda = Car("Škoda", 2014, "Superb II", "Bíla", "Manuální", 310000)
 # print(audi.rok)
 # print(skoda.model)
 print(audi.Vypis())
+print(skoda.cena_bez_dph())
